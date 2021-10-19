6a880e2fdc6eb25dfae05acc32a7c2e18b278ce9
Ethan Halprin <ethan.halprin@gmail.com>
2021-10-19 19:07:59 +0300

    First commit - form example with picker, toggle, textField and button action

--- SwiftUI-Form/ContentView.swift
+++ SwiftUI-Form/ContentView.swift
@@ -8,12 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var username: String = ""
+    @State var isPassphraseOn: Bool = true {
+        didSet {
+            print("Toggle: Private Account ON")
+        }
+    }
+    @State var pushEnabled: Bool = false
+    @State private var pickerIndex = 0
+    var pushTypes = ["Badge", "Sound", "Silent"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("PROFILE")) {
+                    TextField("Username", text: $username)
+                    Toggle(isOn: $isPassphraseOn) {
+                        Text("Requires Password")
+                    }
+                }
+                
+                Section(header: Text("LIVE UPDATES")) {
+                    Toggle(isOn: $pushEnabled) {
+                        Text("Push Notifications")
+                    }
+                    Picker(selection: $pickerIndex, label: Text("Show Previews")) {
+                        ForEach(0 ..< pushTypes.count) {
+                            Text(self.pushTypes[$0])
+                        }
+                    }
+                }
+                
+                Section(header: Text("ABOUT")) {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("1.2.3")
+                    }
+                }
+                
+                Section {
+                    Button(action: {
+                        print("Reset All Settings Pressed")
+                        isPassphraseOn = true
+                        pushEnabled = false
+                        pickerIndex = 0
+                    }) {
+                        Text("Reset All Settings")
+                    }
+                }
+            }
+            .navigationBarTitle("Settings")
+        }
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
