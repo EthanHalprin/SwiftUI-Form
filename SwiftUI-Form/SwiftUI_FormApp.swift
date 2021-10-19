ce72685d0fd4325fbb8cce15e2fa45af95fbd371
Ethan Halprin <ethan.halprin@gmail.com>
2021-10-19 18:31:14 +0300

    Initial Commit

---
+++ SwiftUI-Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI-Form
+//
+//  Created by Ethan on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
